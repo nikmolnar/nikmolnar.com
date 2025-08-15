d6f4357bf6243e5d09b45939c41bd06a28f2a784
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2025-08-15 10:38:19 -0700

    Fix site config for GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.BASE_PATH
+
 // https://astro.build/config
 export default defineConfig({
+  base: base || '',
   vite: { plugins: [tailwindcss()] },
 })
